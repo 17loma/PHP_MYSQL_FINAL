470fe1b5f90544ad3c48a48cb959ccad8951aea6
17loma <lopezalvaro0317@gmail.com>
2025-02-13 22:22:43 +0100

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Detectar automáticamente la URL base en localhost
-$project_folder = "/PHP/PHP_2EVAU/practica final"; // Ajusta esto según tu estructura
-$base_url = "http://localhost" . $project_folder . "/";
-
-// Definir una constante con la URL base
-define("BASE_URL", rtrim($base_url, '/') . "/");
-?>
